327bb15f1e15c37749f765a718bf41813b1c9162
Sachin <bhutekar.cs@gmail.com>
2021-08-08 21:09:38 +0530

    Added comments to caesar-cipher in day8

--- Day8/main.py
+++ Day8/main.py
@@ -1,35 +1,40 @@
 from art import logo
 print(logo)
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 
 def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
-    if char.isalpha() == True:
-      position = alphabet.index(char)
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
-    else:
-      end_text += char
-  print(f"Here's the {cipher_direction}d result: {end_text}")
+    end_text = ""
+    if cipher_direction == "decode":
+        # if we want to decode the text then we need to substratct the position , so multiply by -1.
+        shift_amount *= -1
+    for char in start_text:
+        # check if chracter is alphabet or special character
+        if char.isalpha() == True:
+            # for characters, shift the position as per direction
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            # add shifed character in the string
+            end_text += alphabet[new_position]
+        else:
+            # for special characters, don't shift the position, keep them as it is.
+            end_text += char
+    print(f"Here's the {cipher_direction} result: {end_text}")
+
 
 should_continue = True
 while should_continue:
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    # To keep single shift if user enters shift > 26
+    shift = shift % 26
 
-  shift = shift % 26
-
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  result = input("Type Yes if you want to continue. Otherwise type No: ").lower()
-  if result == "no":
-    should_continue = False
-    print("GoodBye")
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    result = input(
+        "Type Yes if you want to continue. Otherwise type No: ").lower()
+    if result == "no":
+        should_continue = False
+        print("GoodBye")
